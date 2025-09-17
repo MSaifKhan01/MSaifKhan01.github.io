fd1a9629ce0253da4025089a9d16c86483205711
MSaifKhan01 <msaifkhan5038@gmail.com>
2025-09-17 23:59:17 +0530

    modal added on send message button

--- contact-serverApi/server.js
+++ contact-serverApi/server.js
@@ -47,28 +47,28 @@ app.post("/send-email", async (req, res) => {
 
   try {
     // Transporter setup for Gmail
-    // let transporter = nodemailer.createTransport({
-    //   service: "gmail",
-    //   auth: {
-    //     user: process.env.EMAIL_USER,
-    //     pass: process.env.EMAIL_PASS,
-    //   },
-    // });
-const transporter = nodemailer.createTransport({
-  host:"smtp.gmail.com",
-//   host: "smtp.privateemail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS, 
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+// const transporter = nodemailer.createTransport({
+//   host:"smtp.gmail.com",
+// //   host: "smtp.privateemail.com",
+//   port: 587,
+//   secure: false,
+//   requireTLS: true,
+//   auth: {
+//     user: process.env.EMAIL_USER, 
+//     pass: process.env.EMAIL_PASS, 
  
-  },
-    tls: {
-    rejectUnauthorized: false // Bypass SSL certificate validation
-  }
-});
+//   },
+//     tls: {
+//     rejectUnauthorized: false // Bypass SSL certificate validation
+//   }
+// });
 
 
     // Email options - send from the user's email
