12ba5d6ad1751a1365ef8777415524b3086f1968
MSaifKhan01 <msaifkhan5038@gmail.com>
2025-09-21 19:38:04 +0530

    fixing email issue

--- contact-serverApi/server.js
+++ contact-serverApi/server.js
@@ -42,24 +42,24 @@ app.post("/send-email", async (req, res) => {
   console.log("==========",process.env.EMAIL_USER, process.env.EMAIL_PASS)
 
   try {
-    // // // Transporter setup for Gmail
-    // let transporter = nodemailer.createTransport({
-    //   service: "gmail",
-    //   auth: {
-    //     user: process.env.EMAIL_USER,
-    //     pass: process.env.EMAIL_PASS,
-    //   },
-    // });
-
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
+    // // Transporter setup for Gmail
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
     });
+
+
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.gmail.com",
+    //   port: 587,
+    //   auth: {
+    //     user: process.env.EMAIL_USER,
+    //     pass: process.env.EMAIL_PASS,
+    //   },
+    // });
 
 
 
