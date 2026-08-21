2e540b3259ee8e819aaeeaeaa463693fde27887f
MSaifKhan01 <msaifkhan5038@gmail.com>
2026-08-21 15:46:44 +0530

    Resolve th issue for show ui

--- V2_ReactJs/src/main.jsx
+++ V2_ReactJs/src/main.jsx
@@ -1,19 +1,4 @@
-// import "@vitejs/plugin-react/preamble";
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { App } from "./App";
-// import { ThemeProvider } from "./context/ThemeContext";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <ThemeProvider>
-//       <App />
-//     </ThemeProvider>
-//   </React.StrictMode>
-// );
-
+import "@vitejs/plugin-react/preamble";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -28,3 +13,18 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { App } from "./App";
+// import { ThemeProvider } from "./context/ThemeContext";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <ThemeProvider>
+//       <App />
+//     </ThemeProvider>
+//   </React.StrictMode>
+// );
