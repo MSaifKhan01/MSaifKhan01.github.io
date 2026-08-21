acc9458e1ea19473bdbbf8df59681491fec5304e
MSaifKhan01 <msaifkhan5038@gmail.com>
2026-08-21 14:54:43 +0530

    Updated React Portfolio

--- V2_ReactJs/vite.config.js
+++ V2_ReactJs/vite.config.js
@@ -1,19 +1,18 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
+
+
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // export default defineConfig({
 //   plugins: [react()],
+//   base: '/',
 // });
-
-
-
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  
-  plugins: [react()],
-  base: '/',
-});
