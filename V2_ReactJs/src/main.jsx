2de8313fdaa913ee4754e0c6aecef7dc437980c0
MSaifKhan01 <msaifkhan5038@gmail.com>
2026-08-21 15:02:07 +0530

    Fix preamble and rebuild production assets

--- V2_ReactJs/src/main.jsx
+++ V2_ReactJs/src/main.jsx
@@ -1,4 +1,19 @@
-import "@vitejs/plugin-react/preamble";
+// import "@vitejs/plugin-react/preamble";
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { App } from "./App";
+// import { ThemeProvider } from "./context/ThemeContext";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <ThemeProvider>
+//       <App />
+//     </ThemeProvider>
+//   </React.StrictMode>
+// );
+
 
 import React from "react";
 import ReactDOM from "react-dom/client";
--- V2_ReactJs/vite.config.js
+++ V2_ReactJs/vite.config.js
@@ -1,18 +1,16 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
 });
-
-
-
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/',
-// });
