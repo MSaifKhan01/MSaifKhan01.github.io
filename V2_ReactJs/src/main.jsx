bc8f5cd5a557d8e58086a9d51b685556b53560a7
MSaifKhan01 <msaifkhan5038@gmail.com>
2026-08-28 21:54:03 +0530

    Resolved the ui issue

--- V2_ReactJs/src/main.jsx
+++ V2_ReactJs/src/main.jsx
@@ -1,20 +1,5 @@
 // import "@vitejs/plugin-react/preamble";
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import { ThemeProvider } from "./context/ThemeContext";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
-
-
 // import React from "react";
 // import ReactDOM from "react-dom/client";
 // import { App } from "./App";
@@ -28,3 +13,35 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 //     </ThemeProvider>
 //   </React.StrictMode>
 // );
+
+
+// // import React from "react";
+// // import ReactDOM from "react-dom/client";
+// // import { App } from "./App";
+// // import { ThemeProvider } from "./context/ThemeContext";
+// // import "./index.css";
+
+// // ReactDOM.createRoot(document.getElementById("root")).render(
+// //   <React.StrictMode>
+// //     <ThemeProvider>
+// //       <App />
+// //     </ThemeProvider>
+// //   </React.StrictMode>
+// // );
+
+
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { ThemeProvider } from "./context/ThemeContext";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
--- V2_ReactJs/vite.config.js
+++ V2_ReactJs/vite.config.js
@@ -1,24 +1,7 @@
-// // import { defineConfig } from "vite";
-// // import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// // export default defineConfig({
-// //   plugins: [react()],
-// // });
-
-
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/",
-// });
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-base: "./"
-});
+})
